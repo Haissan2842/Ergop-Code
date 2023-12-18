8de29ab9f0693c25aae78bb0603e17c97cd9df93
Haissan2842 <jesuissilver@gmail.com>
2023-12-18 17:05:39 +0100

    page d'acuueil presque responive

--- index.php
+++ index.php
@@ -24,25 +24,25 @@
 
       <div class="container mt-4">
         <div class="row">
-          <div class="col-6 col-lg-3">
+          <div class="col-6 col-lg-3 mb-4">
             <div class="card">
               <img src="<?php echo get_template_directory_uri() ?>/Images/Catégorie-chaises.png" class="card-img-top customcard" alt="Image 1" style="box-shadow: -10px -10px rgba(209, 78, 0, 0.5)">
             </div>
           </div>
       
-          <div class="col-6 col-lg-3">
+          <div class="col-6 col-lg-3 mb-4">
             <div class="card">
               <img src="<?php echo get_template_directory_uri() ?>/Images/Catégorie-bureautique.png" class="card-img-top customcard" alt="Image 2" style="box-shadow: -10px -10px rgba(62, 47, 28, 0.5)">
             </div>
           </div>
       
-          <div class="col-6 col-lg-3">
+          <div class="col-6 col-lg-3 mb-4">
             <div class="card">
               <img src="<?php echo get_template_directory_uri() ?>/Images/Catégorie-meubles.png" class="card-img-top customcard" alt="Image 3" style="box-shadow: -10px -10px rgba(42, 37, 201, 0.5)">
             </div>
           </div>
       
-          <div class="col-6 col-lg-3">
+          <div class="col-6 col-lg-3 mb-4">
             <div class="card">
               <img src="<?php echo get_template_directory_uri() ?>/Images/Catégorie-Accesoires.png" class="card-img-top customcard" alt="Image" 4 style="box-shadow: -10px -10px rgba(46, 127, 27, 0.5)">
             </div>
@@ -52,50 +52,30 @@
       <br><br><br><br><br><br><br><br><br>
     </section>
     <section id="section4">
-<!--
-        <div class="row">
-      
-          <div class="col-md-6">
-            <div class="card">
-              <img src="Images/Article du mois.jpg" class="card-img-top" alt="Image de la carte de gauche">
-            </div>
-          </div>
-      
-          <div class="col-md-6">
-            <div class="card">
-              <div class="card-body">
-                <h1 class="card-title">Chaise de Bureau Gaming Secret Lab</h1>
-                <p class="card-text">La chaise gaming Secret Lab est le choix ultime pour les passionnés de jeux qui recherchent un mélange parfait de confort, de style et de fonctionnalité. Conçue avec une attention méticuleuse aux détails, cette chaise offre un soutien ergonomique exceptionnel grâce à son design ergonomique et ses multiples réglages. Son revêtement en cuir PU de haute qualité ajoute une touche de luxe tout en étant facile à entretenir. Dotée d'un mécanisme d'inclinaison, de coussins ajustables et d'accoudoirs personnalisables, la chaise gaming Secretlab s'adapte à chaque utilisateur, assurant une expérience de jeu immersive et confortable. Son design élégant et moderne en fait également un ajout esthétique à n'importe quel espace de jeu. Que vous soyez en train de jouer pendant des heures ou de travailler, la chaise gaming Secret Lab offre le support dont vous avez besoin pour rester concentré et à l'aise.</p>
-                <a href="#" class="btn btn-primary btn-block">Bouton</a>
-              </div>
-            </div>
-          </div>
-  -->
 
-      <div class="articledumoi">
-          <div class="image-article">
-            <img src="<?php echo get_template_directory_uri(); ?>/Images/Article du mois.jpg" width="90%">
-          </div>
-          <div class="nom-article">
-            <h1>Chaise de Bureau Gaming Secret Lab</h1>
-            <div class="prix">
-              <h2>749.99€</h2>
-              <h1>649.99€</h1>
-            </div>
-          </div>
-          <div class="description">
-            <p>La chaise gaming Secret Lab est le choix ultime pour les passionnés de jeux qui recherchent un mélange parfait de confort, de style et de fonctionnalité. Conçue avec une attention méticuleuse aux détails, cette chaise offre un soutien ergonomique exceptionnel grâce à son design ergonomique et ses multiples réglages. Son revêtement en cuir PU de haute qualité ajoute une touche de luxe tout en étant facile à entretenir. Dotée d'un mécanisme d'inclinaison, de coussins ajustables et d'accoudoirs personnalisables, la chaise gaming Secretlab s'adapte à chaque utilisateur, assurant une expérience de jeu immersive et confortable. Son design élégant et moderne en fait également un ajout esthétique à n'importe quel espace de jeu. Que vous soyez en train de jouer pendant des heures ou de travailler, la chaise gaming Secret Lab offre le support dont vous avez besoin pour rester concentré et à l'aise.
-            </p>
-          </div>
-            <div class="lien">
-              <a href="index.php/article">
-                <div class="rectangle-noir">
-                  <h2>Je vais voir <img src="<?php echo get_template_directory_uri() ?>/Images/entrer.png" width="10%"></h2>
-              </div>
-            </a>
-          </div>
+    <div class="container">
+  <div class="row">
+
+    <!-- Image Article du Mois -->
+    <div class="col-md-6 mb-4">
+      <div class="card">
+        <img src="<?php echo get_template_directory_uri() ?>/Images/Article du mois.jpg" class="card-img-top" alt="Image">
       </div>
+    </div>
 
+    <!-- Texte Article du Mois -->
+    <div class="col-md-6">
+      <div class="card">
+        <div class="card-body couleur_des_sections">
+          <h1 class="card-title">Chaise de bureau Secret Lab</h1>
+          <p class="card-text">La chaise gaming Secret Lab est le choix ultime pour les passionnés de jeux qui recherchent un mélange parfait de confort, de style et de fonctionnalité. Conçue avec une attention méticuleuse aux détails, cette chaise offre un soutien ergonomique exceptionnel grâce à son design ergonomique et ses multiples réglages. Son revêtement en cuir PU de haute qualité ajoute une touche de luxe tout en étant facile à entretenir. Dotée d'un mécanisme d'inclinaison, de coussins ajustables et d'accoudoirs personnalisables, la chaise gaming Secretlab s'adapte à chaque utilisateur, assurant une expérience de jeu immersive et confortable. Son design élégant et moderne en fait également un ajout esthétique à n'importe quel espace de jeu. Que vous soyez en train de jouer pendant des heures ou de travailler, la chaise gaming Secret Lab offre le support dont vous avez besoin pour rester concentré et à l'aise.</p>
+          <a href="<?php echo get_home_url() ?>/index.php/product/chaise-de-bureau-gaming-secret-lab" class="btn btn-outline-dark btn-block">Je vais voir</a>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
 
         <br><br><br><br><br><br><br>
     </section>
@@ -108,7 +88,7 @@
         <div class="container mt-4">
   <div class="row">
     <!-- Première rangée de cartes -->
-    <div class="col-6 col-lg-3">
+    <div class="col-6 col-lg-3 mb-4">
       <div class="card">
       <img src="<?php echo get_template_directory_uri() ?>/Images/Article du mois.jpg " class="card-img-top" alt="Image 1">
       <div class="card-body">
@@ -116,7 +96,7 @@
         </div>
       </div>
     </div>
-    <div class="col-6 col-lg-3">
+    <div class="col-6 col-lg-3 mb-4">
       <div class="card">
         <img src="<?php echo get_template_directory_uri() ?>/Images/Article du mois.jpg " class="card-img-top" alt="Image 2">
         <div class="card-body">
@@ -125,7 +105,7 @@
       </div>
     </div>
 
-    <div class="col-6 col-lg-3">
+    <div class="col-6 col-lg-3 mb-4">
       <div class="card">
         <img src="<?php echo get_template_directory_uri() ?>/Images/Article du mois.jpg " class="card-img-top" alt="Image 3">
         <div class="card-body">
@@ -133,7 +113,7 @@
         </div>
       </div>
     </div>
-    <div class="col-6 col-lg-3">
+    <div class="col-6 col-lg-3 mb-4">
       <div class="card">
         <img src="<?php echo get_template_directory_uri() ?>/Images/Article du mois.jpg " class="card-img-top" alt="Image 4">
         <div class="card-body">
@@ -144,7 +124,7 @@
   </div>
   <div class="row">
     <!-- Deuxième rangée de cartes -->
-    <div class="col-6 col-lg-3">
+    <div class="col-6 col-lg-3 mb-4">
       <div class="card">
         <img src="<?php echo get_template_directory_uri() ?>/Images/Article du mois.jpg " class="card-img-top" alt="Image 5">
         <div class="card-body">
@@ -153,7 +133,7 @@
       </div>
     </div>
 
-    <div class="col-6 col-lg-3">
+    <div class="col-6 col-lg-3 mb-4">
       <div class="card">
         <img src="<?php echo get_template_directory_uri() ?>/Images/Article du mois.jpg " class="card-img-top" alt="Image 6">
         <div class="card-body">
@@ -162,7 +142,7 @@
       </div>
     </div>
 
-    <div class="col-6 col-lg-3">
+    <div class="col-6 col-lg-3 mb-4">
       <div class="card">
         <img src="<?php echo get_template_directory_uri() ?>/Images/Article du mois.jpg " class="card-img-top" alt="Image 7">
         <div class="card-body">
@@ -171,7 +151,7 @@
       </div>
     </div>
 
-    <div class="col-6 col-lg-3">
+    <div class="col-6 col-lg-3 mb-4">
       <div class="card ">
         <img src="<?php echo get_template_directory_uri() ?>/Images/Article du mois.jpg " class="card-img-top" alt="Image 8">
         <div class="card-body">
