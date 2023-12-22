b68a4e24a2f93c5b4f5d0616430700019b5140b5
Haissan2842 <jesuissilver@gmail.com>
2023-12-22 11:51:02 +0100

    footer done

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
 <div class="container mt-4">
   <div class="row">
     <!-- Carte de Gauche -->
-    <div class="col-md-6">
+    <div class="col-md-7">
       <div class="card background_color_footer customcard color_white">
         <div class="card-body">
           <u><h2>Mentions Légales :</h2></u>
@@ -19,7 +19,7 @@
           <br>
           <br>
           <p>
-            Ergop SRL : <br>
+          <u>Ergop SRL : <br>
             Mail :</u> ergopsrl@info.be <br>
             <u> BCE : </u> 0779618003 <br>
             <u> Siège social :</u> Avenue Brugmann n°499 <br>
@@ -30,85 +30,63 @@
       </div>
     </div>
 
-    <!-- Carte de Droite -->
-    <div class="col-md-6">
-      <div class="card background_color_footer customcard">
+<!-- Carte de Droite -->
+<div class="col-md-5 col-sm-12 col-xs-4">
+    <div class="card background_color_footer customcard">
         <div class="card-body">
+        <br><br><br><br><br>
+        <div class="container">
 
-          <!-- Row pour les sous-cartes -->
-          <div class="row">
-            <!-- Première carte imbriquée -->
-            <div class="col-md-5">
-              <div class="card mb-3 background_color_footer">
-                <img src="<?php echo get_template_directory_uri() ?>/Images/Logo X.png" class="customcard" width="50%">
-              </div>
+            <!-- Row pour les sous-cartes -->
+            <div class="row">
+                <!-- Première carte imbriquée -->
+                <div class="col-4 mb-4">
+                    <div class="card background_color_footer border_footer">
+                        <a href="https://x.com"><img src="<?php echo get_template_directory_uri() ?>/Images/Logo X.png" class="customcard " width="50%"></a>
+                    </div>
+                </div>
+
+                <!-- Deuxième carte imbriquée -->
+                <div class="col-4 mb-4">
+                    <div class="card background_color_footer border_footer">
+                        <a href="https://instagram.com"><img src="<?php echo get_template_directory_uri() ?>/Images/Logo instagram.png" class="customcard" width="50%"></a>
+                    </div>
+                </div>
+
+                <!-- Troisième carte imbriquée -->
+                <div class="col-4 mb-4">
+                    <div class="card background_color_footer border_footer">
+                    </div>
+                </div>
+
+                <!-- Cinquième carte imbriquée -->
+                <div class="col-4 mb-4">
+                    <div class="card mb-4 background_color_footer border_footer">
+                        <a href="https://facebook.com"><img src="<?php echo get_template_directory_uri() ?>/Images/Logo Facebook.png" class="customcard" width="50%"></a>
+                    </div>
+                </div>
+
+                <!-- Sixième carte imbriquée -->
+                <div class="col-4 mb-4">
+                    <div class="card background_color_footer border_footer">
+                            <a href="https://linkedin.com"><img src="<?php echo get_template_directory_uri() ?>/Images/logo linkedin.png" class="customcard" width="50%"></a>
+                        </div>
+                    </div>
+                </div>
+
+                <button class="btn btn-outline-light btn-block "><a href="<?php echo home_url() ?>/index.php/contact "><br><h2>Contactez-nous</h2><br></a></button>
             </div>
-
-            <!-- Deuxième carte imbriquée -->
-            <div class="col-md-5">
-              <div class="card mb-3 background_color_footer">
-                <img src="<?php echo get_template_directory_uri() ?>/Images/Logo instagram.png" class="customcard" width="50%">
-              </div>
-            </div>
-
-            <!-- Troisième carte imbriquée -->
-            <div class="col-md-5">
-              <div class="card mb-3 background_color_footer">
-                <img src="<?php echo get_template_directory_uri() ?>/Images/Logo Facebook.png" class="customcard" width="50%">
-              </div>
-            </div>
-
-            <!-- Quatrième carte imbriquée -->
-            <div class="col-md-5">
-              <div class="card mb-3 background_color_footer">
-                <img src="<?php echo get_template_directory_uri() ?>/Images/Logo linkedin.png" class="customcard" width="50%">
-              </div>
-            </div>
-          </div>
-
-          <br><br><br><br>
-          <button class="btn btn-outline-light btn-block"><a href="<?php echo home_url() ?>/index.php/contact ">Contactez-nous</a></button>
         </div>
-      </div>
     </div>
-  </div>
+</div>
+</div>
+</div>
 </div>
 
-
-
-
-
-  <!--
-      <div class="footergrid">
-        <div class="Texte">
-          <u><h2>Mentions Légales :</h2>
-            <br>
-            <br>
-            <a href="<?php echo home_url() ?>/index.php/conditions_generales"><h4>Conditions Générales</h4></a>
-            <br>
-            <a href="<?php echo home_url() ?>/index.php/vie_privee"><h4>Vie Privée</h4></a>
-            <br>
-            <a href="<?php echo home_url() ?>/index.php/droit_de_retractation"><h4>Droit de Rétractation</h4></a>
-            <br>
-            <br>
-              <p>
-                Ergop SRL : <br>
-                Mail :</u> ergopsrl@info.be <br>
-                <u> BCE : </u> 0779618003 <br>
-                <u> Siège social :</u> Avenue Brugmann n°499 <br>
-                <u> TVA :</u> BE0779.618.001
-              </p> </div>
-        <div class="Images">
-          <div class="X-logo"> <img src="<?php echo get_template_directory_uri() ?>/Images/Logo X.png" width="80%"> </div>
-          <div class="Instagram-logo"> <img src="<?php echo get_template_directory_uri() ?>/Images/Logo instagram.png" width="80%"> </div>
-          <div class="linkedin-logo"> <img src="<?php echo get_template_directory_uri() ?>/Images/logo linkedin.png" width="80%"> </div>
-          <div class="facebook-logo"> <img src="<?php echo get_template_directory_uri() ?>/Images/Logo Facebook.png" width="80%"> </div>
-          <div class="Conactez-nous"> <a href="<?php echo home_url() ?>/index.php/contact"><img src="<?php echo get_template_directory_uri() ?>/Images/Contactez nous.png" width="80%"></a> </div>
-        </div>
-      </div>
--->
       <?php wp_footer(); ?>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script> 
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 </body>
 </html>
--- header.php
+++ header.php
@@ -11,7 +11,7 @@
     <nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top">
         <div class="container-fluid container" >
           <a class="navbar-brand" href="<?php echo home_url() ?>"> <img src="<?php echo get_template_directory_uri() ?>/Images/ERGOP - logo pc.png"> </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" id="navbar-toggler">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -29,6 +29,12 @@
                 <a class="nav-link" href="<?php echo home_url() ?>/index.php/shop/">Bureautique</a>
               </li>
             </ul>
+            <!--
+            <form class="d-flex" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+            -->
             <a href="<?php echo home_url() ?>/index.php/shop/"><img src="<?php echo get_template_directory_uri() ?>/Images/Icone search.png" class="hop"></a>
             <h1>&nbsp;&nbsp;</h1>
             <a href="<?php echo home_url() ?>/index.php/compte_connection/"><img src="<?php echo get_template_directory_uri() ?>/Images/Icone compte.png" class="hop"></a>
@@ -38,5 +44,7 @@
         </div>
       </nav>
 
+
+      
 </head>
 <body>
