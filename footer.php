ab392e71c45bc645c3ecfd991e69a8a220b0f5ed
Grum08 <simonderauw14@gmail.com>
2023-12-22 12:53:18 +0100

    yy

--- footer.php
+++ footer.php
@@ -72,38 +72,6 @@
     </div>
   </div>
 </div>
-
-
-
-
-
-  <!--
-      <div class="footergrid">
-        <div class="Texte">
-          <u><h2>Mentions Légales :</h2>
-            <br>
-            <br>
-            <a href="<?php echo home_url() ?>/index.php/conditions_generales"><h4>Conditions Générales</h4></a>
-            <br>
-            <a href="<?php echo home_url() ?>/index.php/vie_privee"><h4>Vie Privée</h4></a>
-            <br>
-            <a href="<?php echo home_url() ?>/index.php/droit_de_retractation"><h4>Droit de Rétractation</h4></a>
-            <br>
-            <br>
-              <p>
-                Ergop SRL : <br>
-                Mail :</u> ergopsrl@info.be <br>
-                <u> BCE : </u> 0779618003 <br>
-                <u> Siège social :</u> Avenue Brugmann n°499 <br>
-                <u> TVA :</u> BE0779.618.001
-              </p> </div>
-        <div class="Images">
-          <div class="X-logo"> <img src="<?php echo get_template_directory_uri() ?>/Images/Logo X.png" width="80%"> </div>
-          <div class="Instagram-logo"> <img src="<?php echo get_template_directory_uri() ?>/Images/Logo instagram.png" width="80%"> </div>
-          <div class="linkedin-logo"> <img src="<?php echo get_template_directory_uri() ?>/Images/logo linkedin.png" width="80%"> </div>
-          <div class="facebook-logo"> <img src="<?php echo get_template_directory_uri() ?>/Images/Logo Facebook.png" width="80%"> </div>
-          <div class="Conactez-nous"> <a href="<?php echo home_url() ?>/index.php/contact"><img src="<?php echo get_template_directory_uri() ?>/Images/Contactez nous.png" width="80%"></a> </div>
-        </div>
       </div>
 -->
       <?php wp_footer(); ?>
